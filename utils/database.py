77cd3467b662604458ce41ceef79cb872c7441a1
0x5b-5245444143544544-5d <vignesh.natarajan2021@vitstudent.ac.in>
2022-02-06 12:43:38 +0530

    Tables are created if they don't already exist, fix bug with tuple parsing.

--- utils/database.py
+++ utils/database.py
@@ -5,7 +5,8 @@ c = sqlite3.connect('data.db')
 
 def create_table() -> None:
     """ Creates table for use by the application. """
-    query = '''CREATE TABLE tasks
+    
+    query = '''CREATE TABLE IF NOT EXISTS tasks
     (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     TODO_CONTENT TEXT NOT NULL,
     IS_COMPLETE INTEGER NOT NULL DEFAULT FALSE);'''
@@ -18,18 +19,20 @@ def add_task(reminder_text: str) -> None:
     c.execute(query)
     c.commit()
 
-def get_task(id: int, is_complete: bool = False) -> list:
+def get_tasks(is_complete: bool) -> list:
     """ Fetches incomplete tasks, optionally completed tasks. """
-    query = f'SELECT * FROM tasks WHERE id={id} AND IS_COMPLETE={int(is_complete)};'
-    cursor = c.execute()
+    query = f'SELECT * FROM tasks WHERE IS_COMPLETE={int(is_complete)};'
+    cursor = c.execute(query)
     response = []
     for result in cursor:
         response.append({
-            'id': result['ID'],
-            'task_content': result['TODO_CONTENT'],
-            'is_complete': bool(result['IS_COMPLETE'])
+            'id': result[0],
+            'task_content': result[1],
+            'is_complete': bool(result[2])
         })
     return response
 
+def modify_task(id: int, is_complete: bool) -> None:
+    """ Toggle whether task is complete or not. """
 
 create_table()
